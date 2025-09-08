451a70e451c952b86671379b9489e2464e90951e
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-08 17:05:36 +0300

    adding some colors

--- lib/constants/my_color.dart
+++ lib/constants/my_color.dart
@@ -1,7 +1,11 @@
-import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
 
 class MyColor {
-  static const Color myblue = Color(
-    0xFF2B479E,
-  );
+  static const Color myblue = Color(0xFF2B479E);
+  static const Color myTextColor = Colors.white;
+  static const Color mySecondaryTextColor =
+      Colors.white70;
+  static const Color myHighlightColor = Color(
+    0xFFFFD700,
+  ); // Gold
 }
