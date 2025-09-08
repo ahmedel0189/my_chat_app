cb4e5f98240066f5c4bf0a50e952c1fff330f5f6
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-08 14:58:21 +0300

    My First Chat App: A_Chat

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AChat extends StatelessWidget {
+  const AChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+void main() {
+  runApp(const AChat());
+}
