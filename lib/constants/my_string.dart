d34b36fedf2667e8d01d78ac1e3a43293b952a99
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-13 16:47:39 +0300

    modifying my Strings

--- lib/constants/my_string.dart
+++ lib/constants/my_string.dart
@@ -1,3 +1,5 @@
+const String appName= 'Scholar Chat';
 const String loginScreenroute='/';
 const String signUpScreenroute='/sign_up_screen';
 const String homePageroute='/home_page_screen';
+const String logiImage='assets/images/scholar.png';
