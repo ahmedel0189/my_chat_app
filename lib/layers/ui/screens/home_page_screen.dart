cc1b4930ef6c98dbd9715e0555d65a24ca4742be
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-09-13 16:48:43 +0300

    Establish the basuc of the homescreen

--- lib/layers/ui/screens/home_page_screen.dart
+++ lib/layers/ui/screens/home_page_screen.dart
@@ -1,3 +1,6 @@
+import 'package:a_chat/constants/my_string.dart';
+import 'package:a_chat/layers/ui/widgets/custom_appbar.dart';
+import 'package:a_chat/layers/ui/widgets/widgets_of_home_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePageScreen extends StatelessWidget {
@@ -5,6 +8,15 @@ class HomePageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: CustomAppbar(appBarTitle: appName),
+      body: Column(
+        children: [
+          Expanded(child: ListView(children: [])),
+          WidgetsOfHomePage()
+              .buildMassageTextField(),
+        ],
+      ),
+    );
   }
 }
